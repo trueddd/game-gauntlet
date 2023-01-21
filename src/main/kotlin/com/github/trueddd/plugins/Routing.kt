36d4c95218c7fec5911aedda4af352b7aa759a1f
trueddd <dkuzmichn1ce@gmail.com>
2023-01-21 20:17:39 +0300

    added static content serving

--- src/main/kotlin/com/github/trueddd/plugins/Routing.kt
+++ src/main/kotlin/com/github/trueddd/plugins/Routing.kt
@@ -1,14 +1,20 @@
 package com.github.trueddd.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        static("/icons") {
+            staticRootFolder = File("src/main/resources/icons")
+            files(".")
         }
     }
 }
