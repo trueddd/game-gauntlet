6392e27527541164dc5e5dcc3ef279709a9b118b
trueddd <dkuzmichn1ce@gmail.com>
2024-03-10 15:24:47 +0300

    fixed ApplicationTest

--- backend/src/jvmTest/kotlin/com/github/trueddd/ApplicationTest.kt
+++ backend/src/jvmTest/kotlin/com/github/trueddd/ApplicationTest.kt
@@ -3,6 +3,7 @@ package com.github.trueddd
 import com.github.trueddd.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -11,6 +12,11 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            authentication {
+                // Installing auth plugin for private routes
+                basic {
+                }
+            }
             configureRouting()
         }
         client.get("/icons/1.png").apply {
